a60ffccf8025cae7bf0b7d71826626880a0ee25f
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-09-10 16:49:10 +0100

    Added melting temperature calculation class.

--- synbiochem/build/lcr.py
+++ synbiochem/build/lcr.py
@@ -1,17 +1,35 @@
-# import collections
+'''
+lcr (c) University of Manchester 2015
+
+lcr is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import collections
 import itertools
+import sys
+from synbiochem.build import melting_temp_utils
+
+reagent_concs = melting_temp_utils.DEFAULT_REAGENT_CONCS
+melting_temp_calculator = \
+    melting_temp_utils.MeltingTempCalculator(reagent_concs)
 
 
-def get_dominos(melting_temp, sequences, plasmid_seq=None, shuffle=False):
+def get_bridging_oligos(target_melt_temp, sequences, plasmid_seq=None,
+                        shuffle=False):
+    '''Designs dominos (bridging oligos) for LCR.'''
     num_sequences = len(sequences)
-    orderings = sorted([list(a) for a in set(itertools.permutations(range(num_sequences)))]) \
+    perms = itertools.permutations(range(num_sequences))
+    orderings = sorted([list(a) for a in set(perms)]) \
         if shuffle \
         else [range(num_sequences)]
 
     if plasmid_seq is not None:
         sequences.append(plasmid_seq)
         for ordering in orderings:
-            ordering.insert(0, num_sequences) 
+            ordering.insert(0, num_sequences)
             ordering.append(num_sequences)
 
     print orderings
@@ -19,21 +37,81 @@ def get_dominos(melting_temp, sequences, plasmid_seq=None, shuffle=False):
     pairs = []
 
     for ordering in orderings:
-        pairs.extend(pairwise(ordering))
+        pairs.extend(_pairwise(ordering))
 
     print pairs
-    # print collections.Counter(pairs)
+    print collections.Counter(pairs)
     print [a for a in sorted(set(pairs))]
-    
+
     for pair in sorted(set(pairs)):
-        print pair[0], pair[1], sequences[pair[0]][-3:], sequences[pair[1]][:3], sequences[pair[0]][-3:] + sequences[pair[1]][:3]
+        reverse, reverse_tm = \
+            _get_bridge(sequences[pair[0]], False, target_melt_temp)
+        forward, forward_tm = \
+            _get_bridge(sequences[pair[1]], True, target_melt_temp)
+        print pair[0], pair[1], reverse, forward, reverse_tm, forward_tm, \
+            reverse + forward if reverse is not None and forward is not None \
+            else ''
 
 
-def pairwise(iterable):
+def _pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return itertools.izip(a, b)
+    first, second = itertools.tee(iterable)
+    next(second, None)
+    return itertools.izip(first, second)
 
 
-get_dominos(60, ['saaaaae', 'sccccce', 'sggggge'], 'sttttte', False)
+def _get_bridge(sequence, forward, target_melt_temp):
+    '''Gets half of bridging oligo.'''
+    for i in range(len(sequence)):
+        subsequence = sequence[:(i+1)] if forward else sequence[-(i+1):]
+        melting_temp = melting_temp_calculator.get_melting_temp(subsequence)
+
+        if melting_temp > target_melt_temp:
+            return subsequence, melting_temp
+
+    return None, -float('inf')
+
+
+def _read_file(filename):
+    melting_temp = 70
+    sequences = []
+    plasmid_seq = None
+    shuffle = False
+
+    in_plasmid = False
+    seq = None
+
+    with open(filename) as fle:
+        for line in fle:
+            if line.startswith('MELTING_TEMP:'):
+                melting_temp = float(line.replace('MELTING_TEMP:', '').strip())
+            elif line.startswith('SHUFFLE:'):
+                shuffle = line.replace('MELTING_TEMP:', '').strip() == 'True'
+            elif line.startswith('>PLASMID'):
+                in_plasmid = True
+
+                if seq is not None:
+                    sequences.append(seq)
+
+                seq = []
+            elif line.startswith('>'):
+                if seq is not None:
+                    sequences.append(seq)
+
+                seq = []
+            elif len(line) > 0 and seq is not None:
+                seq.append(line)
+
+    return melting_temp, sequences, plasmid_seq, shuffle
+
+
+def main(argv):
+    '''main method'''
+    melting_temp, sequences, plasmid_seq, shuffle = _read_file(argv[1])
+    get_bridging_oligos(melting_temp, sequences, plasmid_seq, shuffle)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+# get_bridging_oligos(70, ['saaaaae', 'sccccce', 'sggggge'], 'sttttte', True)
---
+++ synbiochem/build/melting_temp_utils.py
@@ -0,0 +1,64 @@
+'''
+Created on 10 Sep 2015
+
+@author: neilswainston
+'''
+import math
+import re
+
+NA = 'na'
+K = 'k'
+TRIS = 'tris'
+MG = 'mg'
+DNTP = 'dntp'
+
+DEFAULT_REAGENT_CONCS = {NA: 0.05, K: 0, TRIS: 0, MG: 0.002, DNTP: 1e-7}
+
+
+class MeltingTempCalculator(object):
+    '''Class to calculate melting temperatures.'''
+    def __init__(self, reagent_concs):
+        '''See von Ahsen N, Wittwer CT, Schutz E: Oligonucleotide melting
+        temperatures under PCR conditions: deoxynucleotide Triphosphate and
+        Dimethyl sulfoxide concentrations with comparison to alternative
+        empirical formulas. Clin Chem 2001, 47:1956-1961.'''
+        na_equivalence_parameter = 3.79
+
+        na_equivalence = reagent_concs[NA] + reagent_concs[K] \
+            + reagent_concs[TRIS] / 2 \
+            + na_equivalence_parameter \
+            * math.sqrt(reagent_concs[MG] - reagent_concs[DNTP])
+
+        # See Wetmur J: DNA probes: applications of the principles of nucleic
+        # acid hybridization. Crit Rev Biochem Mol Biol 1991, 26:227-259.
+        self.base_melting_temp = 81.5 + 16.6 \
+            * math.log10(na_equivalence / (1.0 + 0.7 * na_equivalence))
+
+    def get_melting_temp(self, dna1, dna2=None):
+        '''Calculates melting temperarure of DNA sequence against its complement,
+        or against second DNA sequence.'''
+        if dna2 is None:
+            dna_gc_content = len(re.findall('G|C', dna1.upper()))
+            return self._get_melting_temp(dna_gc_content, 0, len(dna1))
+        else:
+            # Not implemented...
+            return -1
+
+    def _get_melting_temp(self, dna_gc_content, mismatches, length):
+        '''Calculates melting temperarure.'''
+        gc_increment = 41
+        mismatch_decrement = 100
+        fixed_decrement = 500
+        return self.base_melting_temp + \
+            ((gc_increment * dna_gc_content) - fixed_decrement -
+             (mismatch_decrement * mismatches)) / length
+
+
+def main():
+    '''COMMENT'''
+    melting_temp_calc = MeltingTempCalculator(DEFAULT_REAGENT_CONCS)
+    print melting_temp_calc.get_melting_temp('agcgcgcgggctttct')
+
+
+if __name__ == '__main__':
+    main()
