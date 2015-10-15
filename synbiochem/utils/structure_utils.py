7b08e4b69922be9569f42aba775c2f41a76ac5be
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-15 13:42:17 +0100

    Added structure_utils for PDB analysis.

---
+++ synbiochem/utils/structure_utils.py
@@ -0,0 +1,66 @@
+'''
+synbiochem (c) University of Manchester 2015
+
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+from Bio.PDB.PDBParser import PDBParser
+from matplotlib.colors import LinearSegmentedColormap
+import numpy
+import pylab
+import scipy.spatial
+import sys
+
+
+def calc_proximity(pdb_filename):
+    '''Calculates residue proximities from PDB file.'''
+    pdb_id = pdb_filename.split('.')[0]
+    parser = PDBParser()
+
+    chains = [c
+              for c in parser.get_structure(pdb_id, pdb_filename).get_chains()]
+
+    coords = [residue.child_dict['CA'].get_coord()
+              for residue in chains[0] if 'CA' in residue.child_dict]
+
+    return scipy.spatial.distance.cdist(coords, coords, 'euclidean')
+
+
+def plot(values, plot_filename, plot_format, title, max_value=None):
+    '''Plots 3d matrix values.'''
+    fig = pylab.figure()
+    sub_plot = fig.add_subplot(111)
+
+    cmap = LinearSegmentedColormap.from_list(name='name', colors=['g', 'w'],
+                                             N=10)
+
+    cax = sub_plot.imshow(values, interpolation='nearest', cmap=cmap)
+    cax.set_clim(0.0, max_value)
+    sub_plot.set_title(title)
+
+    # Add colorbar, make sure to specify tick locations to match desired tick
+    # labels
+    min_val = numpy.min(values)
+    max_val = numpy.max(values)
+    cbar = fig.colorbar(cax, ticks=[min_val, max_val])
+    cbar.set_ticks([min_val, max_val])
+
+    pylab.savefig(plot_filename, format=plot_format)
+
+
+def main(argv):
+    '''main method.'''
+    pdb_filename = argv[1]
+    proximities = calc_proximity(pdb_filename)
+
+    plot_format = 'png'
+    plot_filename = pdb_filename.replace('pdb', plot_format)
+    plot(proximities, plot_filename, plot_format,
+         pdb_filename + ' proximity plot')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
