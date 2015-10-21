95ea925f583cc02bc5b382a4a60a3e1573f6e33d
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-21 16:57:09 +0100

    Updated RBS Calculator simulated annealing algorithm.

--- synbiochem/build/rbs.py
+++ synbiochem/build/rbs.py
@@ -1,148 +1,107 @@
 '''
-Created on 20 Oct 2015
+synbiochem (c) University of Manchester 2015
 
-@author: neilswainston
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
 '''
 import math
 import random
+import synbiochem.optimisation.simulated_annealing as sim_ann
 import sys
 import RBS_Calculator
 import RBS_MC_Design
 
+# Necessary to get constants hidden as class variables in RBS_Calculator:
 _RBS_CALC = RBS_Calculator.RBS_Calculator('A', [0, 0])
 
 
-def optimise(sequences, tir_target=None,
-             dg_target=None, max_iter=10000):
-    '''Simulated annealing method for optimising RBS.'''
+class RBSSolution(object):
+    '''Solution for RBS optimisation.'''
+    def __init__(self, sequences, tir_target=None,
+                 dg_target=None):
+        # Check if dg_total or TIR (translation initiation rate) was specified.
+        # If TIR, then convert to dg_total.
+        self.__dg_target = _RBS_CALC.RT_eff * \
+            (_RBS_CALC.logK - math.log(float(tir_target))) \
+            if tir_target is not None else dg_target
 
-    # Initialization
-    counter = 0
-    accepts = 0
-    rejects = 0
-    r_t = 0.6
+        # If an initial RBS (sequences[1] is given, use it.
+        # Otherwise, randomly choose one that is a decent starting point.
+        if sequences[1] is None:
+            (sequences[1], _) = \
+                RBS_MC_Design.GetInitialRBS(sequences[0], sequences[2],
+                                            self.__dg_target)
 
-    # Define the energy/cost function based on the dg_target and the other,
-    # optional targets
-    dg_target, energy, calc = _init(sequences, tir_target, dg_target)
+        self.__sequences = sequences
+        self.__energy = self.__calc_energy(sequences[1])
+        self.__rbs_new = None
+        self.__energy_new = None
 
-    while energy > 0.25 and counter < max_iter:
-        counter += 1
-        accepted = False
+    def get_energy(self):
+        '''Gets the (simulated annealing) energy.'''
+        return self.__energy
 
-        rbs_new, energy_new = _mutate(sequences, dg_target)
+    def mutate(self, verbose=False):
+        '''Mutates and scores RBS.'''
+        weighted_moves = [('insert', 0.1), ('delete', 0.1), ('replace', 0.8)]
+        move = RBS_MC_Design.weighted_choice(weighted_moves)
+        pos = int(random.random() * len(self.__sequences[1]))
 
-        if RBS_MC_Design.calc_constraints(rbs_new, calc):
-            energy_new = float('inf')
+        if move == 'insert':
+            letter = random.choice(['A', 'T', 'G', 'C'])
+            rbs_new = self.__sequences[1][0:pos] + letter + \
+                self.__sequences[1][pos:len(self.__sequences[1])]
+        if move == 'delete' and len(self.__sequences[1]) > 1:
+            rbs_new = self.__sequences[1][0:pos] + \
+                self.__sequences[1][pos+1:len(self.__sequences[1])]
+        if move == 'replace':
+            letter = random.choice(['A', 'T', 'G', 'C'])
+            rbs_new = self.__sequences[1][0:pos] + letter + \
+                self.__sequences[1][pos + 1:len(self.__sequences[1])]
 
-        print 'New energy = ', energy_new
+        self.__rbs_new = RBS_MC_Design.RemoveStartCodons(rbs_new)
+        len_rbs_new = len(self.__rbs_new)
 
-        if energy_new < energy:
-            # Accept move immediately
-            sequences[1] = rbs_new
-            energy = energy_new
-            accepted = True
-            print 'Move immediately accepted'
-        elif math.exp((energy - energy_new) / r_t) > random.random():
-            # Accept move based on conditional probability
-            sequences[1] = rbs_new
-            energy = energy_new
-            accepts += 1
-            accepted = True
-            print 'Move conditionally accepted'
-        else:
-            # Reject move
-            rejects += 1
+        if len_rbs_new > RBS_MC_Design.Max_RBS_Length:
+            self.__rbs_new = \
+                self.__rbs_new[len_rbs_new -
+                               RBS_MC_Design.Max_RBS_Length:len_rbs_new+1]
 
-        if accepted:
-            calc.print_dG(calc.infinity)
+        self.__energy_new = self.__calc_energy(rbs_new, verbose)
+        return self.__energy_new
 
-        # Simulated annealing control
-        if accepts + rejects > 50:
-            ratio = float(accepts) / float(accepts + rejects)
+    def accept(self):
+        '''Accept potential update.'''
+        self.__sequences[1] = self.__rbs_new
+        self.__energy = self.__energy_new
+        self.__rbs_new = None
+        self.__energy_new = None
 
-            if ratio > 0.2:
-                # Too many accepts, reduce rt
-                r_t /= 2.0
-                accepts = 0
-                rejects = 0
+    def __calc_energy(self, rbs, verbose=False):
+        '''Calculates (simulated annealing) energy for given RBS.'''
+        calc = RBS_MC_Design.Run_RBS_Calculator(self.__sequences[0],
+                                                self.__sequences[2],
+                                                rbs,
+                                                verbose)
 
-                print 'Accepting too many conditional moves, reducing ' + \
-                    'temperature'
+        return calc.dG_total_list[0] - self.__dg_target
 
-            elif ratio < 0.01:
-                # Too many rejects, increase rt
-                r_t *= 2.0
-                accepts = 0
-                rejects = 0
+    def __repr__(self):
+        # return '%r' % (self.__dict__)
+        return self.__sequences[1]
 
-                print 'Rejecting too many conditional moves, increasing ' + \
-                    'temperature'
-
-    if tir_target is not None:
-        # Convert back to TIR (Translation Initiation Rate)
-        return (_RBS_CALC.K * math.exp(-calc.dG_total_list[0] /
-                                       _RBS_CALC.RT_eff), sequences[1],
-                counter)
-    else:
-        return (calc.dG_total_list[0], sequences[1], counter)
-
-
-def _init(sequences, tir_target, dg_target):
-    '''Initialises the simulated annealing job.'''
-    # Check if dg_total or TIR (translation initiation rate) was specified.
-    # If TIR, then convert to dg_total.
-    dg_target = _RBS_CALC.RT_eff * \
-        (_RBS_CALC.logK - math.log(float(tir_target))) \
-        if tir_target is not None else dg_target
-
-    # If rbs_Init is given, use it. Otherwise, randomly choose one that is a
-    # decent starting point.
-    if sequences[1] is None:
-        (sequences[1], calc) = \
-            RBS_MC_Design.GetInitialRBS(sequences[0], sequences[2], dg_target)
-    else:
-        calc = RBS_MC_Design.Run_RBS_Calculator(sequences[0], sequences[2],
-                                                sequences[1])
-
-    energy = calc.dG_total_list[0] - dg_target
-    calc.print_dG(calc.infinity)
-
-    return dg_target, energy, calc
-
-
-def _mutate(sequences, dg_target):
-    '''Mutates and scores RBS.'''
-    weighted_moves = [('insert', 0.1), ('delete', 0.1), ('replace', 0.8)]
-    move = RBS_MC_Design.weighted_choice(weighted_moves)
-    pos = int(random.random() * len(sequences[1]))
-
-    if move == 'insert':
-        letter = random.choice(['A', 'T', 'G', 'C'])
-        rbs_new = sequences[1][0:pos] + letter + \
-            sequences[1][pos:len(sequences[1])]
-    if move == 'delete' and len(sequences[1]) > 1:
-        rbs_new = sequences[1][0:pos] + sequences[1][pos + 1:len(sequences[1])]
-    if move == 'replace':
-        letter = random.choice(['A', 'T', 'G', 'C'])
-        rbs_new = sequences[1][0:pos] + letter + \
-            sequences[1][pos + 1:len(sequences[1])]
-
-    rbs_new = RBS_MC_Design.RemoveStartCodons(rbs_new)
-
-    if len(rbs_new) > RBS_MC_Design.Max_RBS_Length:
-        rbs_new = rbs_new[
-            len(rbs_new) - RBS_MC_Design.Max_RBS_Length:len(rbs_new) + 1]
-
-    calc = RBS_MC_Design.Run_RBS_Calculator(
-        sequences[0], sequences[2], rbs_new, True)
-
-    return rbs_new, calc.dG_total_list[0] - dg_target
+    def __print__(self):
+        return self.__repr__
 
 
 def main(argv):
     '''main method.'''
-    print optimise([argv[1], None, argv[2]], tir_target=float(argv[3]))
+    print sim_ann.optimise(RBSSolution([argv[1], None, argv[2]],
+                                       tir_target=float(argv[3])),
+                           verbose=False)
 
 
 if __name__ == '__main__':
---
+++ synbiochem/optimisation/simulated_annealing.py
@@ -0,0 +1,65 @@
+'''
+synbiochem (c) University of Manchester 2015
+
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import math
+import random
+
+
+def optimise(solution, max_iter=10000, verbose=False):
+    '''Optmises a solution with simulated annealing.'''
+
+    # Initialization:
+    counter = 0
+    accepts = 0
+    rejects = 0
+    r_temp = 0.6
+
+    energy = solution.get_energy()
+
+    print str(counter) + '\t' + str(solution) + '\t' + \
+        str(energy)
+
+    while energy > 0.25 and counter < max_iter:
+        counter += 1
+        energy_new = solution.mutate(verbose=verbose)
+
+        if energy_new < energy:
+            # Accept move immediately:
+            solution.accept()
+            energy = energy_new
+            print str(counter) + '\t' + str(solution) + '\t' + \
+                str(energy)
+        elif energy == energy_new:
+            # Take no action:
+            continue
+        elif math.exp((energy - energy_new) / r_temp) > random.random():
+            # Accept move based on conditional probability:
+            solution.accept()
+            energy = energy_new
+            print str(counter) + '\t' + str(solution) + '\t' + \
+                str(energy)
+            accepts += 1
+        else:
+            # Reject move:
+            rejects += 1
+
+        # Simulated annealing control:
+        if accepts + rejects > 50:
+            if float(accepts) / float(accepts + rejects) > 0.2:
+                # Too many accepts, reduce r_temp:
+                r_temp /= 2.0
+                accepts = 0
+                rejects = 0
+            if float(accepts) / float(accepts + rejects) < 0.01:
+                # Too many rejects, increase r_temp:
+                r_temp *= 2.0
+                accepts = 0
+                rejects = 0
+
+    return (solution, counter)
