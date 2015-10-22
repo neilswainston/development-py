68d8d1a69756e953f956eefc95b9ae59c1d00459
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-22 11:02:33 +0100

    Added CDS mutation.

--- synbiochem/build/rbs.py
+++ synbiochem/build/rbs.py
@@ -13,6 +13,7 @@ import sys
 
 import RBS_Calculator
 import RBS_MC_Design
+import synbiochem.utils.sequence_utils as seq_util
 import synbiochem.optimisation.simulated_annealing as sim_ann
 
 
@@ -23,26 +24,28 @@ _RBS_CALC = RBS_Calculator.RBS_Calculator('A', [0, 0])
 class RBSSolution(object):
     '''Solution for RBS optimisation.'''
 
-    def __init__(self, post_seq, len_target=50, tir_target=None,
-                 dg_target=None):
+    def __init__(self, prot_seq, taxonomy_id, len_target=50, tir_target=None):
         # Check if dg_total or TIR (translation initiation rate) was specified.
         # If TIR, then convert to dg_total.
         self.__dg_target = _RBS_CALC.RT_eff * \
-            (_RBS_CALC.logK - math.log(float(tir_target))) \
-            if tir_target is not None else dg_target
+            (_RBS_CALC.logK - math.log(float(tir_target)))
+
+        self.__prot_seq = prot_seq
+        self.__cod_opt = seq_util.CodonOptimiser(taxonomy_id)
+        cds = self.__cod_opt.get_codon_optimised_seq(self.__prot_seq)
 
         # If an initial RBS (sequences[1] is given, use it.
         # Otherwise, randomly choose one that is a decent starting point.
-        (rbs, _) = RBS_MC_Design.GetInitialRBS('', post_seq,
+        (rbs, _) = RBS_MC_Design.GetInitialRBS('', cds,
                                                self.__dg_target)
 
         pre_seq = \
             ''.join([random.choice(['A', 'T', 'G', 'C'])
                      for _ in range(0, max(0, len_target - len(rbs)))])
 
-        self.__sequences = [pre_seq, rbs, post_seq]
+        self.__sequences = [pre_seq, rbs, cds]
         self.__dg = self.__calc_dg(rbs)
-        self.__sequences_new = [None, None, post_seq]
+        self.__sequences_new = [None, None, cds]
         self.__dg_new = None
 
     def get_energy(self):
@@ -61,6 +64,7 @@ class RBSSolution(object):
         '''Mutates and scores whole design.'''
         self.__mutate_pre_seq()
         self.__mutate_rbs()
+        self.__mutate_cds()
         self.__dg_new = self.__calc_dg(self.__sequences_new[1], verbose)
         return self.__dg_new - self.__dg_target
 
@@ -118,6 +122,12 @@ class RBSSolution(object):
         self.__sequences_new[0] = pre_seq_new
         self.__sequences_new[1] = rbs_new
 
+    def __mutate_cds(self):
+        '''Mutates CDS.'''
+        self.__sequences_new[2] = \
+            self.__cod_opt.mutate(self.__prot_seq, self.__sequences[2],
+                                  1.0/len(self.__sequences[2]))
+
     def __repr__(self):
         # return '%r' % (self.__dict__)
         return str(self.__dg) + '\t' + str(self.get_tir()) + '\t' + \
@@ -140,8 +150,9 @@ def _rand_nuc():
 
 def main(argv):
     '''main method.'''
-    print sim_ann.optimise(RBSSolution(argv[1], len_target=int(argv[2]),
-                                       tir_target=float(argv[3])),
+    print sim_ann.optimise(RBSSolution(argv[1], argv[2],
+                                       len_target=int(argv[3]),
+                                       tir_target=float(argv[4])),
                            verbose=False)
 
 
