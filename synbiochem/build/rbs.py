04cf35033f6f3251d49f70cb52fbed8012ab3415
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-22 17:27:41 +0100

    Added CAI to scoring function.

--- synbiochem/build/rbs.py
+++ synbiochem/build/rbs.py
@@ -45,42 +45,36 @@ class RBSSolution(object):
             ''.join([random.choice(['A', 'T', 'G', 'C'])
                      for _ in range(0, max(0, len_target - len(rbs)))])
 
-        self.__sequences = [pre_seq, rbs, cds]
+        self.__seqs = [pre_seq, rbs, cds]
         self.__dg = self.__calc_dg(rbs)
-        self.__sequences_new = [None, None, cds]
+        self.__seqs_new = [None, None, cds]
         self.__dg_new = None
 
-    def get_energy(self):
+    def get_energy(self, d_g=None, cds=None):
         '''Gets the (simulated annealing) energy.'''
-        return self.__dg - self.__dg_target
-
-    def get_dg(self):
-        '''Gets the delta G.'''
-        return self.__dg
-
-    def get_tir(self):
-        '''Gets the translation initiation rate.'''
-        return _RBS_CALC.K * math.exp(-self.__dg / _RBS_CALC.RT_eff)
+        d_g = self.__dg if d_g is None else d_g
+        cds = self.__seqs[2] if cds is None else cds
+        return abs(d_g - self.__dg_target) * self.__cod_opt.get_cai(cds)
 
     def mutate(self, verbose=False):
         '''Mutates and scores whole design.'''
         self.__mutate_pre_seq()
         self.__mutate_rbs()
         self.__mutate_cds()
-        self.__dg_new = self.__calc_dg(self.__sequences_new[1], verbose)
-        return self.__dg_new - self.__dg_target
+        self.__dg_new = self.__calc_dg(self.__seqs_new[1], verbose)
+        return self.get_energy(self.__dg_new, self.__seqs_new[2])
 
     def accept(self):
         '''Accept potential update.'''
-        self.__sequences = self.__sequences_new
+        self.__seqs = self.__seqs_new
         self.__dg = self.__dg_new
-        self.__sequences_new = [None, None, self.__sequences[2]]
+        self.__seqs_new = [None, None, self.__seqs[2]]
         self.__dg_new = None
 
     def __calc_dg(self, rbs, verbose=False):
         '''Calculates (simulated annealing) energy for given RBS.'''
-        calc = RBS_MC_Design.Run_RBS_Calculator(self.__sequences[0],
-                                                self.__sequences[2],
+        calc = RBS_MC_Design.Run_RBS_Calculator(self.__seqs[0],
+                                                self.__seqs[2],
                                                 rbs,
                                                 verbose)
 
@@ -88,55 +82,60 @@ class RBSSolution(object):
 
     def __mutate_pre_seq(self):
         '''Mutates pre-sequence.'''
-        pos = int(random.random() * len(self.__sequences[0]))
-        self.__sequences_new[0] = _replace(self.__sequences[0], pos,
-                                           _rand_nuc())
+        pos = int(random.random() * len(self.__seqs[0]))
+        self.__seqs_new[0] = _replace(self.__seqs[0], pos, _rand_nuc())
 
     def __mutate_rbs(self):
         '''Mutates RBS.'''
         weighted_moves = [('insert', 0.1), ('delete', 0.1), ('replace', 0.8)]
         move = RBS_MC_Design.weighted_choice(weighted_moves)
-        pos = int(random.random() * len(self.__sequences[1]))
+        pos = int(random.random() * len(self.__seqs[1]))
 
         if move == 'insert' and \
-                len(self.__sequences[1]) < RBS_MC_Design.Max_RBS_Length:
+                len(self.__seqs[1]) < RBS_MC_Design.Max_RBS_Length:
             letter = random.choice(['A', 'T', 'G', 'C'])
-            rbs_new = self.__sequences[1][0:pos] + letter + \
-                self.__sequences[1][pos:len(self.__sequences[1])]
-            pre_seq_new = self.__sequences_new[0][1:] \
-                if len(self.__sequences_new[0]) > 0 else ''
+            rbs_new = self.__seqs[1][0:pos] + letter + \
+                self.__seqs[1][pos:len(self.__seqs[1])]
+            pre_seq_new = self.__seqs_new[0][1:] \
+                if len(self.__seqs_new[0]) > 0 else ''
 
-        elif move == 'delete' and len(self.__sequences[1]) > 1:
-            rbs_new = _replace(self.__sequences[1], pos, '')
+        elif move == 'delete' and len(self.__seqs[1]) > 1:
+            rbs_new = _replace(self.__seqs[1], pos, '')
             pre_seq_new = random.choice(['A', 'T', 'G', 'C']) + \
-                self.__sequences_new[0]
+                self.__seqs_new[0]
 
         elif move == 'replace':
-            rbs_new = _replace(self.__sequences[1], pos, _rand_nuc())
-            pre_seq_new = self.__sequences_new[0]
+            rbs_new = _replace(self.__seqs[1], pos, _rand_nuc())
+            pre_seq_new = self.__seqs_new[0]
 
         else:
-            pre_seq_new = self.__sequences_new[0]
-            rbs_new = self.__sequences[1]
+            pre_seq_new = self.__seqs_new[0]
+            rbs_new = self.__seqs[1]
 
         rbs_new = RBS_MC_Design.RemoveStartCodons(rbs_new)
 
-        self.__sequences_new[0] = pre_seq_new
-        self.__sequences_new[1] = rbs_new
+        self.__seqs_new[0] = pre_seq_new
+        self.__seqs_new[1] = rbs_new
 
     def __mutate_cds(self):
         '''Mutates CDS.'''
-        self.__sequences_new[2] = \
-            self.__cod_opt.mutate(self.__prot_seq, self.__sequences[2],
-                                  1.0/len(self.__sequences[2]))
+        self.__seqs_new[2] = \
+            self.__cod_opt.mutate(self.__prot_seq, self.__seqs[2],
+                                  1.0/len(self.__seqs[2]))
 
     def __repr__(self):
         # return '%r' % (self.__dict__)
-        return str(self.get_tir()) + '\t' + self.__sequences[0] + ' ' + \
-            self.__sequences[1] + ' ' + self.__sequences[2]
+        return str(self.__cod_opt.get_cai(self.__seqs[2])) + '\t' + \
+            str(_get_tir(self.__dg)) + '\t' + self.__seqs[0] + ' ' + \
+            self.__seqs[1] + ' ' + self.__seqs[2]
 
     def __print__(self):
         return self.__repr__
+
+
+def _get_tir(d_g):
+    '''Gets the translation initiation rate.'''
+    return _RBS_CALC.K * math.exp(-d_g / _RBS_CALC.RT_eff)
 
 
 def _replace(sequence, pos, nuc):
