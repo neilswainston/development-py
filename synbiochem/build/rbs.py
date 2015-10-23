a4ca33aae2cbbf8a3b5468124a43656ab98d23ea
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-23 12:09:22 +0100

    Multiple CDS sequences to be used in RBS optimisation simultaneously.

--- synbiochem/build/rbs.py
+++ synbiochem/build/rbs.py
@@ -25,20 +25,21 @@ _RBS_CALC = RBS_Calculator.RBS_Calculator('A', [0, 0])
 class RBSSolution(object):
     '''Solution for RBS optimisation.'''
 
-    def __init__(self, uniprot_id, taxonomy_id, len_target=50,
+    def __init__(self, uniprot_id_set, taxonomy_id, len_target=50,
                  tir_target=None):
         # Check if dg_total or TIR (translation initiation rate) was specified.
         # If TIR, then convert to dg_total.
         self.__dg_target = _RBS_CALC.RT_eff * \
             (_RBS_CALC.logK - math.log(float(tir_target)))
 
-        self.__prot_seq = uniprot_utils.get_sequences([uniprot_id])[uniprot_id]
+        self.__prot_seqs = uniprot_utils.get_sequences([uniprot_id_set])
         self.__cod_opt = seq_utils.CodonOptimiser(taxonomy_id)
-        cds = self.__cod_opt.get_codon_optimised_seq(self.__prot_seq)
+        cds = [self.__cod_opt.get_codon_optimised_seq(prot_seq)
+               for prot_seq in self.__prot_seqs.values()]
 
-        # If an initial RBS (sequences[1] is given, use it.
-        # Otherwise, randomly choose one that is a decent starting point.
-        (rbs, _) = RBS_MC_Design.GetInitialRBS('', cds,
+        # Randomly choose an RBS that is a decent starting point,
+        # using the first CDS as the upstream sequence:
+        (rbs, _) = RBS_MC_Design.GetInitialRBS('', cds[0],
                                                self.__dg_target)
 
         pre_seq = \
@@ -46,39 +47,38 @@ class RBSSolution(object):
                      for _ in range(0, max(0, len_target - len(rbs)))])
 
         self.__seqs = [pre_seq, rbs, cds]
-        self.__dg = self.__calc_dg(rbs)
+        self.__dgs = self.__calc_dgs(rbs)
         self.__seqs_new = [None, None, cds]
-        self.__dg_new = None
+        self.__dgs_new = None
 
-    def get_energy(self, d_g=None, cds=None):
+    def get_energy(self, dgs=None, cds=None):
         '''Gets the (simulated annealing) energy.'''
-        d_g = self.__dg if d_g is None else d_g
+        dgs = self.__dgs if dgs is None else dgs
         cds = self.__seqs[2] if cds is None else cds
-        return abs(d_g - self.__dg_target) * self.__cod_opt.get_cai(cds)
+        return abs(dgs[0] - self.__dg_target) * self.__cod_opt.get_cai(cds[0])
 
     def mutate(self, verbose=False):
         '''Mutates and scores whole design.'''
         self.__mutate_pre_seq()
         self.__mutate_rbs()
         self.__mutate_cds()
-        self.__dg_new = self.__calc_dg(self.__seqs_new[1], verbose)
-        return self.get_energy(self.__dg_new, self.__seqs_new[2])
+        self.__dgs_new = self.__calc_dgs(self.__seqs_new[1], verbose)
+        return self.get_energy(self.__dgs_new, self.__seqs_new[2])
 
     def accept(self):
         '''Accept potential update.'''
         self.__seqs = self.__seqs_new
-        self.__dg = self.__dg_new
+        self.__dgs = self.__dgs_new
         self.__seqs_new = [None, None, self.__seqs[2]]
-        self.__dg_new = None
+        self.__dgs_new = None
 
-    def __calc_dg(self, rbs, verbose=False):
-        '''Calculates (simulated annealing) energy for given RBS.'''
-        calc = RBS_MC_Design.Run_RBS_Calculator(self.__seqs[0],
-                                                self.__seqs[2],
-                                                rbs,
-                                                verbose)
-
-        return calc.dG_total_list[0]
+    def __calc_dgs(self, rbs, verbose=False):
+        '''Calculates (simulated annealing) energies for given RBS.'''
+        return [RBS_MC_Design.Run_RBS_Calculator(self.__seqs[0],
+                                                 post_seq,
+                                                 rbs,
+                                                 verbose).dG_total_list[0]
+                for post_seq in self.__seqs[2]]
 
     def __mutate_pre_seq(self):
         '''Mutates pre-sequence.'''
@@ -120,22 +120,24 @@ class RBSSolution(object):
     def __mutate_cds(self):
         '''Mutates CDS.'''
         self.__seqs_new[2] = \
-            self.__cod_opt.mutate(self.__prot_seq, self.__seqs[2],
-                                  1.0/len(self.__seqs[2]))
+            [self.__cod_opt.mutate(prot_seq, dna_seq, 1.0/len(dna_seq))
+             for dna_seq, prot_seq in zip(self.__seqs[2],
+                                          self.__prot_seqs.values())]
 
     def __repr__(self):
         # return '%r' % (self.__dict__)
-        return str(self.__cod_opt.get_cai(self.__seqs[2])) + '\t' + \
-            str(_get_tir(self.__dg)) + '\t' + self.__seqs[0] + ' ' + \
-            self.__seqs[1] + ' ' + self.__seqs[2]
+        return str(self.__cod_opt.get_cai(self.__seqs[2][0])) + '\t' + \
+            str(_get_tirs(self.__dgs)) + '\t' + self.__seqs[0] + ' ' + \
+            self.__seqs[1] + ' ' + str(self.__seqs[2])
 
     def __print__(self):
         return self.__repr__
 
 
-def _get_tir(d_g):
+def _get_tirs(dgs):
     '''Gets the translation initiation rate.'''
-    return _RBS_CALC.K * math.exp(-d_g / _RBS_CALC.RT_eff)
+    return [_RBS_CALC.K * math.exp(-d_g / _RBS_CALC.RT_eff)
+            for d_g in dgs]
 
 
 def _replace(sequence, pos, nuc):
