b0fa12444da04b6c1461b063b9ca850604554940
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-26 17:06:45 +0000

    Updated documentation.

--- synbiochem/utils/chemistry_utils.py
+++ synbiochem/utils/chemistry_utils.py
@@ -1,7 +1,11 @@
 '''
-Created on 6 Oct 2015
+synbiochem (c) University of Manchester 2015
 
-@author: neilswainston
+synbiochem is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
 '''
 # pylint: disable=too-few-public-methods
 
