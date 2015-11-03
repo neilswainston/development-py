ae3b710a88a2b9da87836ba54a23598c2d7392e4
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-11-03 12:03:07 +0000

    Updated reaction balancing module.

--- synbiochem/utils/reaction_utils.py
+++ synbiochem/utils/reaction_utils.py
@@ -10,40 +10,17 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 import itertools
 from scipy.optimize import linprog
 
-import chemistry_utils
+import synbiochem.utils.chemistry_utils as chem_utils
 
 
-def balance():
-    '''balance method.'''
-    ccc = [-1, 4]
-    aaa = [[-3, 1], [1, 2]]
-    bbb = [6, 4]
-    x0_bounds = (None, None)
-    x1_bounds = (-3, None)
-    res = linprog(ccc, A_ub=aaa, b_ub=bbb,
-                  bounds=(x0_bounds, x1_bounds),
-                  options={'disp': True})
-    print res
+def balance(all_formulae, all_charges, optional_formulae=None,
+            optional_charges=None, max_stoich=8):
+    '''Applies linear programming to balance reaction.'''
+    if optional_formulae is None:
+        optional_formulae = ['H', 'H2O']
 
-
-def balance2():
-    '''balance method.'''
-    ccc = [1, 1, 1, 1, 1, 1, 1, 1]
-    aaa = [[1, 5, -3, 0, 0, 0, 0, -1],
-           [2, 4, -3, 0, 1, 0, -1, -2],
-           [0, 7, -3, 1, 2, -1, -2, 0],
-           [0, -1, 1, 1, 0, -1, 0, 0]]
-
-    bbb = [0, 0, 0, 0]
-    bounds = ((1, 8), (1, 8), (1, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8))
-    res = linprog(ccc, A_ub=aaa, b_ub=bbb, bounds=bounds,
-                  options={'disp': True})
-    print res
-
-
-def balance3(all_formulae, all_charges):
-    optional_formulae = ['H', 'H2O']
-    optional_charges = [1, 0]
+    if optional_charges is None:
+        optional_charges = [1, 0]
 
     all_formulae.extend([optional_formulae for _ in range(2)])
     all_charges.extend([optional_charges for _ in range(2)])
@@ -51,39 +28,59 @@ def balance3(all_formulae, all_charges):
                       for formula in formulae]
                      for idx, formulae in enumerate(all_formulae)]
 
-    elements = [elem_comp.keys() for elem_comps in all_elem_comp
-                for elem_comp in elem_comps]
+    a_matrix = _get_elem_matrix(all_elem_comp, all_charges)
 
-    aaa = []
-    for element in set(itertools.chain(*elements)):
-        aaa.append([elem_comp[element] if element in elem_comp else 0
-                    for elem_comps in all_elem_comp
-                    for elem_comp in elem_comps])
+    bounds = [(1, max_stoich)] * (len(a_matrix[0]) - len(optional_formulae) *
+                                  2) + \
+        [(0, max_stoich)] * len(optional_formulae) * 2
 
-    aaa.append(list(itertools.chain(*([charge * (-1 if idx % 2 == 0 else 1)
-                                       for charge in charges]
-                                      for idx,
-                                      charges in enumerate(all_charges)))))
+    res = linprog([1] * len(a_matrix[0]),
+                  A_ub=a_matrix,
+                  b_ub=[0] * (len(a_matrix)),
+                  bounds=bounds)
 
-    ccc = [1] * len(elements)
-    bbb = [0] * len(aaa)
-    bounds = ((1, 4), (1, 4), (1, 4), (0, 4), (0, 4), (0, 4), (0, 4))
-    res = linprog(ccc, A_ub=aaa, b_ub=bbb, bounds=bounds,
-                  options={'disp': True})
-    print res
+    balance = [(x[0], x[1] * y) for x, y in zip([(x, -1 if idx % 2 == 0 else 1)
+                                                 for idx, formulae
+                                                 in enumerate(all_formulae)
+                                                 for x in formulae], res.x)
+               if y > 0.0] if res.success else None
+
+    return res.success, balance
+
 
 def _get_elem_comp(formula, idx):
     '''Returns elemental compositions, multiplying stoichiometry by -1
     if reactants.'''
-    elem_comp = chemistry_utils.get_elem_comp(formula)
-    elem_comp.update((x, y * (-1 if idx % 2 == 0 else 1))
+    elem_comp = chem_utils.get_elem_comp(formula)
+    elem_comp.update((x, y * (1 if idx % 2 == 0 else -1))
                      for x, y in elem_comp.items())
     return elem_comp
 
 
+def _get_elem_matrix(all_elem_comp, all_charges):
+    '''Gets the elemental (and charge) matrix representing rows of elements
+    and columns of compounds.'''
+    a_matrix = []
+    elements = [elem_comp.keys() for elem_comps in all_elem_comp
+                for elem_comp in elem_comps]
+
+    for element in set(itertools.chain(*elements)):
+        a_matrix.append([elem_comp[element] if element in elem_comp else 0
+                         for elem_comps in all_elem_comp
+                         for elem_comp in elem_comps])
+
+    a_matrix.append(list(itertools.chain(*([charge * (1 if idx % 2 == 0
+                                                      else -1)
+                                            for charge in charges]
+                                           for idx, charges
+                                           in enumerate(all_charges)))))
+
+    return a_matrix
+
+
 def main():
     '''main method.'''
-    balance3([['CO2', 'C5H7O4'], ['C3H3O3']], [[0, -1], [-1]])
+    print balance([['CO2', 'C5H7O4'], ['C3H3O3']], [[0, -1], [-1]])
 
 if __name__ == '__main__':
     main()
