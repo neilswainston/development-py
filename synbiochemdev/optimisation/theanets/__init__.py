b86c06278ef9220e43fcf2f83761350aaccaa929
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-11-09 22:02:16 +0000

    Minor update to Classifier.

--- synbiochemdev/optimisation/theanets/__init__.py
+++ synbiochemdev/optimisation/theanets/__init__.py
@@ -30,25 +30,26 @@ class Classifier(object):
         self.__exp = None
         self.__y_map = None
 
-    def train(self, x_data, y_data, split=0.75):
-        '''Train the network.'''
+    def train(self, x_data, y_data, split=0.75, hidden_layers=None):
+        '''Train the network. Accepts list of floats as x_data,
+        and list of anything as y_data.'''
+        if hidden_layers is None:
+            hidden_layers = [2]
 
         # Check lengths of x_data and y_data are equal,
         # assume all tuples in x_data are of the same length.
         assert len(x_data) == len(y_data)
 
-        ind = int(split * len(x_data))
-
-        # Plain neural network with a single hidden layer:
-        self.__exp = theanets.Experiment(theanets.Classifier,
-                                         layers=(len(x_data[0]), 50,
-                                                 len(set(y_data))))
+        layers = [len(x_data[0])] + hidden_layers + [len(set(y_data))]
+        self.__exp = theanets.Experiment(theanets.Classifier, layers=layers)
 
         x_data = np.array(x_data, dtype=np.float32)
         y_enum = _enumerate(y_data)
         y_data = np.array([y[1] for y in y_enum], dtype=np.int32)
         self.__y_map = dict(set(y_enum))
 
+        # Split data into training and validation:
+        ind = int(split * len(x_data))
         self.__exp.train((x_data[:ind], y_data[:ind]),
                          (x_data[ind:], y_data[ind:]),
                          optimize=self.__optimize,
@@ -60,6 +61,7 @@ class Classifier(object):
         y_test = np.array([self.__y_map[y] for y in y_test], dtype=np.int32)
         y_pred = self.__exp.network.classify(x_test)
         inv_y_map = {v: k for k, v in self.__y_map.items()}
+
         return [inv_y_map[y] for y in y_pred], inv_y_map, \
             classification_report(y_test, y_pred), \
             confusion_matrix(y_test, y_pred)
