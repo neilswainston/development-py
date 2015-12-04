c90abcfcc39df306d7a49d24c6a7d0ce4dc754ee
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-12-04 16:05:54 +0000

    Added fungi deep learning exercise.

---
+++ synbiochemdev/learning/fungi/__init__.py
@@ -0,0 +1,9 @@
+'''
+HolyGrail (c) University of Manchester 2015
+
+HolyGrail is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
