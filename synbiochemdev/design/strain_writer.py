3c6bbdc2943077cc7107274e0bf7c6eeaab31416
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-12-21 16:44:50 +0000

    Added group permission addition facility.

--- synbiochemdev/design/strain_writer.py
+++ synbiochemdev/design/strain_writer.py
@@ -9,12 +9,13 @@ import sys
 from synbiochem.utils import ice_utils
 
 
-def main(args):
-    '''main method.'''
-    with io.open(args[0], 'r', newline='\r') as fle:
+def write_strain(filename, ice_url, ice_username, ice_password,
+                 group_name=None):
+    '''Writes strains.'''
+    with io.open(filename, 'r', newline='\r') as fle:
         pairs = [str(line).split() for line in fle]
 
-        ice_client = ice_utils.ICEClient(args[1], args[2], args[3])
+        ice_client = ice_utils.ICEClient(ice_url, ice_username, ice_password)
 
         for pair in pairs:
             host = ice_client.get_ice_entry(pair[0])
@@ -33,8 +34,27 @@ def main(args):
             ice_client.add_link(strain.get_ice_id(), plasmid.get_ice_id())
             ice_client.set_ice_entry(strain)
 
+            if group_name:
+                groups = ice_client.get_groups()
+                ice_client.add_permission(strain.get_ice_id(),
+                                          groups[group_name])
+
             print host.get_ice_id(), plasmid.get_ice_id(), strain.get_ice_id()
 
+
+def set_groups(filename, ice_url, ice_username, ice_password, group_name):
+    '''Sets group permissions.'''
+    with io.open(filename, 'r', newline='\r') as fle:
+        ice_client = ice_utils.ICEClient(ice_url, ice_username, ice_password)
+        groups = ice_client.get_groups()
+
+        for ice_id in fle:
+            ice_client.add_permission(ice_id, groups[group_name])
+
+
+def main(args):
+    '''main method.'''
+    set_groups(*args)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
