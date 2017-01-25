a4e1fdf84eec867f3ad53e98d910e123ff317a9a
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-01-25 14:46:25 +0000

    Removed obsolete code.

--- synbiochemdev/learning/fungi/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-HolyGrail (c) University of Manchester 2015
-
-HolyGrail is licensed under the MIT License.
-
-To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
-
-@author:  neilswainston
-'''
