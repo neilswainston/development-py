6382f820b8f5fbf2c8b9818877887e80f9ebde0d
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-24 13:16:05 +0100

    Removed typenames.

--- design/design.ctrl.js
+++ design/design.ctrl.js
@@ -3,13 +3,12 @@ designApp.controller("designCtrl", function($scope) {
     $scope.models = {
         selected: null,
         templates: [
-        	{type: "design", typeName: "Design", id: 2, "features": []},
-            {type: "feature", typeName: "Feature", id: 1}
+        	{type: "design", id: 2, "features": []},
+            {type: "feature", id: 1}
         ],
         designs: [
         	{
         		type: "design",
-        		typeName: "Design",
         		id: 1, "features": []
         	}
         ]
