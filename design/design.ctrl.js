72895124cef71ee2f7dcc6a9c4168fafd2307700
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-24 15:14:54 +0100

    Added SO templates.

--- design/design.ctrl.js
+++ design/design.ctrl.js
@@ -4,20 +4,59 @@ designApp.controller("designCtrl", function($scope) {
 	self.models = {
         selected: null,
         templates: [
-        	{
-        		type: "design",
-        		id: 2,
-        		features: []
-        	},
             {
+            	type: "feature",
+        		typ: "http://purl.obolibrary.org/obo/SO_0000167",
+        		id: 1,
+        		name: "promoter",
+                temp_params: {
+                   fixed: true
+                }
+            },
+            {
+            	type: "feature",
+                typ: "http://purl.obolibrary.org/obo/SO_0000139",
+                id: 2,
+                name:"ribosome entry site",
+                end: 60,
+                parameters: {
+                   "TIR target": 15000
+                },
+                temp_params: {
+                   fixed: false
+                }
+            },
+            {
+            	type: "feature",
+            	typ: "http://purl.obolibrary.org/obo/SO_0000316",
+                id: 3,
+                name: "coding sequence",
+                options: [
+                   {
+                      typ: "http://purl.obolibrary.org/obo/SO_0000316",
+                      name: "coding sequence",
+                      temp_params: {
+                         fixed: false
+                      }
+                   }
+                ]
+            },
+        	{
         		type: "feature",
-        		id: 1
+        		typ: "http://purl.obolibrary.org/obo/SO_0000141",
+        		id: 4,
+        		name: "terminator",
+                temp_params: {
+                   fixed: true
+                }
             }
         ],
         designs: [
         	{
         		type: "design",
-            	id: 1,
+        		id: 1,
+            	name: "Design",
+                desc: "Design",
             	features: []
         	}
         ]
