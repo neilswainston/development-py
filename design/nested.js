5f3ccea6793fb513e9049022d6a31428fa6438cf
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-24 09:57:44 +0100

    Further simplification.

--- design/nested.js
+++ design/nested.js
@@ -1,15 +1,12 @@
-/**
- * The controller doesn't do much more than setting the initial data model
- */
 angular.module("demo").controller("NestedListsDemoController", function($scope) {
 
     $scope.models = {
         selected: null,
         templates: [
-            {type: "item", typeName: "Component", id: 2},
-            {type: "container", typeName: "Design", id: 1, columns: [[]]}
+            {type: "item", typeName: "Component", id: 1},
+            {type: "container", typeName: "Design", id: 2, columns: [[]]}
         ],
-        dropzone: [
+        designs: [
             {
                 "type": "container",
                 "id": 1,
@@ -18,9 +15,7 @@ angular.module("demo").controller("NestedListsDemoController", function($scope) 
         ]
     };
 
-    $scope.$watch('models.dropzone', function(model) {
+    $scope.$watch('models.designs', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
-
 });
-
