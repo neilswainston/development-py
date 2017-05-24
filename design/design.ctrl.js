0e92a45c47c1078686c22ee0e75beb2ec6ca43d0
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-05-24 14:09:50 +0100

    Refactored controller.

--- design/design.ctrl.js
+++ design/design.ctrl.js
@@ -1,20 +1,34 @@
 designApp.controller("designCtrl", function($scope) {
-
-    $scope.models = {
+	var self = this;
+	
+	self.models = {
         selected: null,
         templates: [
-        	{type: "design", id: 2, "features": []},
-            {type: "feature", id: 1}
+        	{
+        		type: "design",
+        		id: 2,
+        		features: []
+        	},
+            {
+        		type: "feature",
+        		id: 1
+            }
         ],
         designs: [
         	{
         		type: "design",
-        		id: 1, "features": []
+            	id: 1,
+            	features: []
         	}
         ]
     };
+	
+	self.modelAsJson = angular.toJson(self.models, true)
 
-    $scope.$watch('models.designs', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
-    }, true);
+	$scope.$watch(function() {
+		return self.models.designs;
+	},               
+	function(values) {
+		self.modelAsJson = angular.toJson(self.models, true)
+	}, true);
 });
