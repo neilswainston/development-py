fb8ce4a475f97aa440296234b296d08f3151457a
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-06-13 12:26:02 +0100

    Updated calibration curve with real data.

--- synbiochemdev/calibration/calibration.py
+++ synbiochemdev/calibration/calibration.py
@@ -14,40 +14,47 @@ import pandas as pd
 def main(args):
     '''main method.'''
     df = pd.read_table(args[0])
-    df['WEIGHT'] = 1 / df['CONC'] ** 2
     x_range = range(-10, int(max(df['CONC'])))
 
-    unweighted_fit = np.poly1d(np.polyfit(df['CONC'], df['PEAK AREA'], 1))
-    weighted_fit = np.poly1d(np.polyfit(df['CONC'], df['PEAK AREA'], 1,
-                                        w=df['WEIGHT']))
+    weightings = {
+        'Unweighted': np.polyfit(df['CONC'], df['PEAK AREA'], 1),
+        'Weighted (1/x^0.5)': np.polyfit(df['CONC'], df['PEAK AREA'], 1,
+                                         w=(1 / df['CONC'] ** 0.5)),
+        'Weighted (1/x)': np.polyfit(df['CONC'], df['PEAK AREA'], 1,
+                                     w=(1 / df['CONC'])),
+        'Weighted (1/x^2)': np.polyfit(df['CONC'], df['PEAK AREA'], 1,
+                                       w=(1 / df['CONC'] ** 2)),
+    }
 
     plt.figure(1)
     plt.suptitle('Calibration curves')
 
     plt.subplot(121)
-    _subplot(df, x_range, unweighted_fit, weighted_fit)
+    _subplot(df, x_range, weightings)
 
     plt.subplot(122)
-    _subplot(df, x_range, unweighted_fit, weighted_fit)
+    _subplot(df, x_range, weightings)
     axes = plt.gca()
-    axes.set_xlim([-1, 1])
-    axes.set_ylim([-1, 1])
+    axes.set_xlim([-1, 12])
+    axes.set_ylim([-0.25, 0.25])
     plt.show()
 
 
-def _subplot(df, x_range, unweighted_fit, weighted_fit):
+def _subplot(df, x_range, weightings):
     '''Make subplot.'''
     plt.xlabel(df.columns[0])
     plt.ylabel(df.columns[1])
 
     plt.scatter(df['CONC'], df['PEAK AREA'])
 
-    unweighted, = plt.plot(x_range, unweighted_fit(x_range),
-                           c='red', label='Unweighted: ' + str(unweighted_fit))
-    weighted, = plt.plot(x_range, weighted_fit(x_range),
-                         c='green', label='Weighted: ' + str(weighted_fit))
+    handles = []
+    for title, weight in weightings.iteritems():
+        weight = np.poly1d(weight)
+        ret = plt.plot(x_range, weight(x_range), label=title + str(weight))
 
-    plt.legend(handles=[unweighted, weighted])
+        handles.append(ret[0])
+
+    plt.legend(handles=handles)
 
     axes = plt.gca()
     axes.grid(True, which='both')
