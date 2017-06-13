b91c397ba1fc8a2e5c58c36e4e4ab6ce2f49bc68
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-06-13 15:12:14 +0100

    Refactored.

--- synbiochemdev/calibration/calibration.py
+++ synbiochemdev/calibration/calibration.py
@@ -12,28 +12,57 @@ import numpy as np
 import pandas as pd
 
 
-def analyse(idx, max_idx, filename):
+def analyse(df):
     '''Perform analysis.'''
-    df = pd.read_table(filename)
-    x_plot_range = sorted(set(df['CONC'].tolist()))[1] * 1.2
-    x_range = range(-int(math.ceil(x_plot_range)), int(max(df['CONC'])))
+    curves = []
 
-    weightings = [['(1/x^%d)' % coeff,
-                   np.polyfit(df['CONC'], df['PEAK AREA'], 1,
-                              w=(1 / df['CONC'] ** coeff))]
-                  for coeff in np.arange(0, 2, 0.5)]
+    for coeff in np.arange(0, 2, 0.5):
+        curve = np.poly1d(np.polyfit(df['CONC'], df['PEAK AREA'], 1,
+                                     w=(1 / df['CONC'] ** coeff)))
+        ae = np.abs((df['PEAK AREA'] - curve(df['CONC'])) / df['PEAK AREA'])
+        curves.append([coeff, curve, np.mean(ae)])
+
+    return curves
+
+
+def plot(idx, max_idx, df, curves):
+    '''Plot data.'''
+    x_plot_range = sorted(set(df['CONC'].tolist()))[1] * 1.2
 
     plt.subplot(max_idx, 2, idx * 2 + 1)
-    plt.title = filename
-    _subplot(df, x_range, weightings)
+    _subplot(df, curves)
 
     plt.subplot(max_idx, 2, idx * 2 + 2)
-    plt.title = filename
-    _subplot(df, x_range, weightings)
+    _subplot(df, curves)
 
     axes = plt.gca()
     axes.set_xlim([-x_plot_range, x_plot_range])
     axes.set_ylim([-0.025, 0.25])
+
+
+def _subplot(df, curves):
+    '''Make subplot.'''
+    x_plot_range = sorted(set(df['CONC'].tolist()))[1] * 1.2
+    x_range = range(-int(math.ceil(x_plot_range)), int(max(df['CONC'])))
+
+    plt.xlabel(df.columns[0])
+    plt.ylabel(df.columns[1])
+
+    plt.scatter(df['CONC'], df['PEAK AREA'])
+
+    handles = []
+
+    for curve in curves:
+        label = '(1/x^%d): mae=%.3f' % (curve[0], curve[2])
+        ret = plt.plot(x_range, curve[1](x_range), label=label)
+        handles.append(ret[0])
+
+    plt.legend(handles=handles)
+
+    axes = plt.gca()
+    axes.grid(True, which='both')
+    axes.axhline(y=0, color='k')
+    axes.axvline(x=0, color='k')
 
 
 def main(args):
@@ -42,37 +71,11 @@ def main(args):
     plt.suptitle('Calibration curves')
 
     for idx, filename in enumerate(args):
-        analyse(idx, len(args), filename)
+        df = pd.read_table(filename)
+        curves = analyse(df)
+        plot(idx, len(args), df, curves)
 
     plt.show()
-
-
-def _subplot(df, x_range, weightings):
-    '''Make subplot.'''
-    plt.xlabel(df.columns[0])
-    plt.ylabel(df.columns[1])
-
-    plt.scatter(df['CONC'], df['PEAK AREA'])
-
-    handles = []
-
-    peak_area = 0
-
-    for weight in weightings:
-        wgt = np.poly1d(weight[1])
-        ae = np.abs((df['PEAK AREA'] - wgt(df['CONC'])) / df['PEAK AREA'])
-        label = weight[0] + ': mae=%.3f' % np.mean(ae)
-        ret = plt.plot(x_range, wgt(x_range), label=label)
-        handles.append(ret[0])
-
-        print (wgt - peak_area).roots
-
-    plt.legend(handles=handles)
-
-    axes = plt.gca()
-    axes.grid(True, which='both')
-    axes.axhline(y=0, color='k')
-    axes.axvline(x=0, color='k')
 
 
 if __name__ == '__main__':
