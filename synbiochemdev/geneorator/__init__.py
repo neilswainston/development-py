02f71d99b305c0471de6f60d836f63baf585d005
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-07-09 18:46:00 +0100

    Moved GeneORatorGenie code.

--- synbiochemdev/geneorator/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-synbiochemdev (c) University of Manchester 2015
-
-synbiochemdev is licensed under the MIT License.
-
-To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
-
-@author:  neilswainston
-'''
