5b57c82c8b1bc8e85c6699474493cb469d4dd3a0
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-07-26 16:31:13 +0100

    Updated riboswitch_predicter.

--- synbiochemdev/dixon/riboswitch_predicter.sh
+++ synbiochemdev/dixon/riboswitch_predicter.sh
@@ -3,6 +3,5 @@ LENGTH=24
 TAG=MGSSHH
 POST_SEQ=CATCATCATCACAGCAGCGGCCTGGTGCCGCGCGGCAGCCAT
 OUTPUT_FILE=results.xls
-
-pip install synbiochem-py
+sudo pip install synbiochem-py
 python riboswitch_predicter.py $PRE_SEQ $LENGTH $TAG $POST_SEQ $OUTPUT_FILE
