6583525cde81c27d0c0afe3bbedae1238de8966e
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-09-26 10:53:20 +0100

    Included BLAST filter.

--- synbiochemdev/barcode.py
+++ synbiochemdev/barcode.py
@@ -8,17 +8,43 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 @author:  neilswainston
 '''
 # pylint: disable=invalid-name
+import sys
+
 from synbiochem.utils import seq_utils
 
-seqs = [seq_utils.get_random_dna(24, 3) for _ in range(500)]
 
-for query_idx, seq in enumerate(seqs[:-1]):
-    id_seqs_subjects = {'seq_' + str(idx + query_idx + 1): seq
-                        for idx, seq in enumerate(seqs[query_idx + 1:])}
+def get_seqs(num_barcodes, length, max_repeat_nuc):
+    '''Get barcodes.'''
+    seqs = [seq_utils.get_random_dna(length, max_repeat_nuc)
+            for _ in range(num_barcodes)]
 
-    for result in seq_utils.do_blast(id_seqs_subjects, {query_idx: seq},
-                                     word_size=4):
+    for query_idx, seq in enumerate(seqs[:-1]):
+        id_seqs_queries = {query_idx: seq}
+        id_seqs_subjects = {'seq_' + str(idx + query_idx + 1): seq
+                            for idx, seq in enumerate(seqs[query_idx + 1:])}
+
+        do_blast(id_seqs_subjects, id_seqs_queries)
+
+    return seqs
+
+
+def do_blast(subjects, queries, exp_threshold=1):
+    '''Runs BLAST, filtering results.'''
+    for result in seq_utils.do_blast(subjects, queries, word_size=4):
         for alignment in result.alignments:
             for hsp in alignment.hsps:
-                print result.query + '\t' + alignment.hit_def + '\n' + str(hsp)
-                print
+                if hsp.expect < exp_threshold:
+                    print result.query + '\t' + alignment.hit_def + '\n' + str(hsp)
+                    print
+                    return False
+
+    return True
+
+
+def main(args):
+    '''main method.'''
+    get_seqs(int(args[0]), int(args[1]), int(args[2]))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
