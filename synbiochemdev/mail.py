f5f340d7c5ebc7f6de34c3ac67e68ca2cd6ca1e3
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-05-23 00:43:01 +0100

    Shortened script length.

--- synbiochemdev/mail.py
+++ synbiochemdev/mail.py
@@ -1,8 +1,8 @@
 import feedparser as f
 import markovify as m
 
-_TEXT = [[e.title, e.summary]
-         for e in f.parse('feed://www.dailymail.co.uk/articles.rss').entries]
+_T = [[e.title, e.summary]
+      for e in f.parse('feed://www.dailymail.co.uk/articles.rss').entries]
 
 for _ in range(100):
-    print(m.Text([j for sub in _TEXT for j in sub]).make_short_sentence(140))
+    print(m.Text([j for sub in _T for j in sub]).make_short_sentence(140))
