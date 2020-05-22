99bcf21110299a21200efde04d3641762814df03
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-05-23 00:39:08 +0100

    Simplified script.

--- synbiochemdev/mail.py
+++ synbiochemdev/mail.py
@@ -1,12 +1,8 @@
 import feedparser as f
 import markovify as m
 
-text = []
-
-for ent in f.parse('feed://www.dailymail.co.uk/articles.rss').entries:
-    text.extend([ent.title, ent.summary])
-
-text_model = m.Text(text)
+_TEXT = [[e.title, e.summary]
+         for e in f.parse('feed://www.dailymail.co.uk/articles.rss').entries]
 
 for _ in range(100):
-    print(text_model.make_short_sentence(140))
+    print(m.Text([j for sub in _TEXT for j in sub]).make_short_sentence(140))
